7f6fcdc12233249504eb8e82b271404de7f6bc64
engruju <161950075+engruju@users.noreply.github.com>
2025-09-04 00:23:38 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,7 +9,7 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "ap-south-1"
+  region = "us-east-1"
   access_key = var.access_key
   secret_key = var.secret_key
 }
@@ -41,8 +41,8 @@ resource "aws_security_group" "ec2_security_group" {
 }
 
 resource "aws_instance" "Monitoring_server" {
-ami = "ami-00bb6a80f01f03502"  
-instance_type = "t2.medium"
+ami = "ami-0360c520857e3138f"  
+instance_type = "t2.micro"
 security_groups = [aws_security_group.ec2_security_group.name]
 key_name = var.key_name
 tags = {
